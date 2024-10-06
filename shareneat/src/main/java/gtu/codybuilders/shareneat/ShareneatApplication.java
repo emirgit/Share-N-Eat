b84165c5a77b9dc5df2050468693ae9d00a3714a
emirgit <emirtr852816@hotmail.com>
2024-10-06 22:38:20 +0300

    v0.01
    Project is created.

---
+++ shareneat/src/main/java/gtu/codybuilders/shareneat/ShareneatApplication.java
@@ -0,0 +1,13 @@
+package gtu.codybuilders.shareneat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareneatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareneatApplication.class, args);
+	}
+
+}
---
+++ shareneat/src/test/java/gtu/codybuilders/shareneat/ShareneatApplicationTests.java
@@ -0,0 +1,13 @@
+package gtu.codybuilders.shareneat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareneatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
