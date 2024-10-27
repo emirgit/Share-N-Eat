142a0db91159f8a5f28dc4c7ad0ca0a89ea7fe8b
Oğuz Akkuş <oguzakkus@192.168.1.59>
2024-10-27 20:30:09 +0300

    changes made in app.js

--- snefront/src/App.js
+++ snefront/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import UploadPage from './pages/UploadPage';
+
+// import ProductPage from './pages/ProductPage';
+// import ProfilePage from './pages/ProfilePage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+            <Route path="/" element={<MainPage/>} />
+            <Route path="/upload" element={<UploadPage />} />
+                {/* <Route path="/product" component={ProductPage} />
+                <Route path="/profile" component={ProfilePage} /> */}
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
